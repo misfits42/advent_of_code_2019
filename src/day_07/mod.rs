b1152c9d0db74b20c55fc4ab4060dfe26b8ecfbd
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-01-05 15:43:08 +1100

    Refactored Day 07 Part 1 code
    Extracted the running of intcode machines in Day 07 Part 1 into a
    separate function to make the solution function look a whole lot
    neater.

--- src/day_07/mod.rs
+++ src/day_07/mod.rs
@@ -15,65 +15,45 @@ pub fn solution_part_1(filename: String) -> (i32, Vec<i32>) {
     let mut max_output_value = 0;
     let mut max_outputs_phases = vec![-1, -1, -1, -1, -1];
     for combo in phase_combinations {
-        // Initial configuration of amplifier inputs
-        let amp_a_input = queue![combo[0], 0];
-        let mut amp_b_input = queue![];
-        let mut amp_c_input = queue![];
-        let mut amp_d_input = queue![];
-        let mut amp_e_input = queue![];
         // Run amplifier A
-        let mut amp_a = IntcodeMachine::new(initial_memory.clone(), amp_a_input);
-        amp_a.execute_program();
-        let amp_a_output = amp_a.get_output();
-        if amp_a_output.len() != 1 {
-            panic!("Bad Amp A output length! {}", amp_a_output.len());
-        }
-        amp_b_input.add(combo[1]).ok();
-        amp_b_input.add(amp_a_output[0]).ok();
+        let amp_a_output = run_intcode_machine_as_amp(initial_memory.clone(), combo[0], 0);
         // Run amplifier B
-        let mut amp_b = IntcodeMachine::new(initial_memory.clone(), amp_b_input);
-        amp_b.execute_program();
-        let amp_b_output = amp_b.get_output();
-        if amp_b_output.len() != 1 {
-            panic!("Bad Amp B output length! {}", amp_b_output.len());
-        }
-        amp_c_input.add(combo[2]).ok();
-        amp_c_input.add(amp_b_output[0]).ok();
+        let amp_b_output =
+            run_intcode_machine_as_amp(initial_memory.clone(), combo[1], amp_a_output);
         // Run amplifier C
-        let mut amp_c = IntcodeMachine::new(initial_memory.clone(), amp_c_input);
-        amp_c.execute_program();
-        let amp_c_output = amp_c.get_output();
-        if amp_c_output.len() != 1 {
-            panic!("Bad Amp C output length! {}", amp_c_output.len());
-        }
-        amp_d_input.add(combo[3]).ok();
-        amp_d_input.add(amp_c_output[0]).ok();
+        let amp_c_output =
+            run_intcode_machine_as_amp(initial_memory.clone(), combo[2], amp_b_output);
         // Run amplifier D
-        let mut amp_d = IntcodeMachine::new(initial_memory.clone(), amp_d_input);
-        amp_d.execute_program();
-        let amp_d_output = amp_d.get_output();
-        if amp_d_output.len() != 1 {
-            panic!("Bad Amp D output length! {}", amp_d_output.len());
-        }
-        amp_e_input.add(combo[4]).ok();
-        amp_e_input.add(amp_d_output[0]).ok();
+        let amp_d_output =
+            run_intcode_machine_as_amp(initial_memory.clone(), combo[3], amp_c_output);
         // Run amplifier E
-        let mut amp_e = IntcodeMachine::new(initial_memory.clone(), amp_e_input);
-        amp_e.execute_program();
-        let amp_e_output = amp_e.get_output();
-        if amp_e_output.len() != 1 {
-            panic!("Bad Amp E output length! {}", amp_e_output.len());
-        }
-        let output_to_thrusters = amp_e_output[0];
+        let amp_e_output_to_thruster =
+            run_intcode_machine_as_amp(initial_memory.clone(), combo[4], amp_d_output);
         // Check if output is greatest seen so far
-        if output_to_thrusters > max_output_value {
-            println!("Found new max: {} > {} ({:?})", output_to_thrusters, max_output_value, combo);
-            max_output_value = output_to_thrusters;
+        if amp_e_output_to_thruster > max_output_value {
+            println!(
+                "Found new max: {} > {} ({:?})",
+                amp_e_output_to_thruster, max_output_value, combo
+            );
+            max_output_value = amp_e_output_to_thruster;
             max_outputs_phases = combo;
         }
     }
     // Return output value and associated phase setting combination
     return (max_output_value, max_outputs_phases);
+}
+
+/// Runs the given program in an IntcodeMachine instance, using the given phase and input as the two
+/// input values to the machine (prior to program execution).
+fn run_intcode_machine_as_amp(initial_memory: Vec<i32>, phase: i32, input_value: i32) -> i32 {
+    let amp_input = queue![phase, input_value];
+    let mut amp_machine = IntcodeMachine::new(initial_memory, amp_input);
+    amp_machine.execute_program();
+    let amp_output = amp_machine.get_output();
+    if amp_output.len() != 1 {
+        panic!("Bad amplifier output length!");
+    }
+    return amp_output[0];
 }
 
 #[cfg(test)]
@@ -84,20 +64,20 @@ mod tests {
     fn test_p1_ex_input_01() {
         let result = solution_part_1(String::from("./input/day_07/test/test_01.txt"));
         assert_eq!(43210, result.0);
-        assert_eq!(vec![4,3,2,1,0], result.1);
+        assert_eq!(vec![4, 3, 2, 1, 0], result.1);
     }
 
     #[test]
     fn test_p1_ex_input_02() {
         let result = solution_part_1(String::from("./input/day_07/test/test_02.txt"));
         assert_eq!(54321, result.0);
-        assert_eq!(vec![0,1,2,3,4], result.1);
+        assert_eq!(vec![0, 1, 2, 3, 4], result.1);
     }
 
     #[test]
     fn test_p1_ex_input_03() {
         let result = solution_part_1(String::from("./input/day_07/test/test_03.txt"));
         assert_eq!(65210, result.0);
-        assert_eq!(vec![1,0,4,3,2], result.1);
+        assert_eq!(vec![1, 0, 4, 3, 2], result.1);
     }
 }
